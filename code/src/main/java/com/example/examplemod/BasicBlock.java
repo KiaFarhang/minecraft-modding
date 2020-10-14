78209ac24351455a002c81f6bc062b47428cc6e5
Kia Farhang <kfarhang0@gmail.com>
2020-10-13 20:31:32 -0700

    feature: add BasicBlock class

---
+++ code/src/main/java/com/example/examplemod/BasicBlock.java
@@ -0,0 +1,10 @@
+package com.example.examplemod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BasicBlock extends Block{
+    public BasicBlock () {
+		super(Block.Properties.create(Material.ROCK));
+	}
+}
